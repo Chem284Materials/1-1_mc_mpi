2e258e826744b62f2e746357e580879dbd79e853
Taylor Barnes <taylor.a.barnes@gmail.com>
2026-01-13 14:06:34 -0500

    General improvements to problem

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,18 @@ int main(int argc, char *argv[]) {
   long npoints = 100000000;
 
   long points_in_circle = 0;
-  for (int ipoint=0; ipoint < npoints; ipoint++) {
+  for (int ipoint=0; ipoint < npoints; ++ipoint) {
     double x = dist(mt);
     double y = dist(mt);
 
     // Square of distance between (x, y) and the origin
     double r2 =  (x * x) + (y * y);
 
-    if ( r2 <= 1.0 ) points_in_circle++;
+    if ( r2 <= 1.0 ) ++points_in_circle;
   }
 
   // Value of pi
   double pi = ( 4.0 * points_in_circle ) / npoints;
 
-  std::cout << "Estimated value of pi: " << pi << std::endl;
+  std::cout << "Estimated value of pi: " << pi << '\n';
 }
